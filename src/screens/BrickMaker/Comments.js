adab6db52086189bf7930800153cd03369339c60
Francois Rulliere <francoisrulliere@SG-CEM-028.local>
2019-12-02 19:58:47 +0800

    add image feature

--- src/screens/BrickMaker/Comments.js
+++ src/screens/BrickMaker/Comments.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import { Text } from 'react-native-elements';
 import moment from 'moment';
 
@@ -17,16 +17,31 @@ const styles = StyleSheet.create({
   container: { marginTop: 10 }
 });
 
+const IS_URL = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/;
+const IS_IMAGE = /\.(jpeg|jpg|gif|png)$/;
+const isTextLinkImage = (text: string) =>
+  text.match(IS_URL) != null && text.match(IS_IMAGE) != null;
+
 const Comment = (props: { comment: CommentT }) => {
   const { comment } = props;
   const user = useUser(comment.author);
+  const isImage = isTextLinkImage(comment.text); // url.match(/\.(jpeg|jpg|gif|png)$/) != null
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.author}>{user.email}</Text>
         <Text>{moment(comment.datetime.toDate()).fromNow()}</Text>
       </View>
-      <Text>{comment.text}</Text>
+      {isImage ? (
+        <Image
+          style={{ width: 66, height: 58 }}
+          source={{
+            uri: comment.text
+          }}
+        />
+      ) : (
+        <Text>{comment.text}</Text>
+      )}
     </View>
   );
 };

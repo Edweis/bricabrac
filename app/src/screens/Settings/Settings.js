65aa76ee7a3e989254ffe3b9d5aa47f00891b2b8
Francois Rulliere <francoisrulliere@SG-CEM-028.local>
2019-12-16 08:47:38 +0800

    fix #42

--- app/src/screens/Settings/Settings.js
+++ app/src/screens/Settings/Settings.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { Linking } from 'expo';
 import firebase from '../../firebase';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
 const GITHUB_LINK = 'https://github.com/Edweis/bricabrac/issues';
 const Settings = () => {
   return (
-    <View>
-      <ListItem
-        leftIcon={{ name: 'logo-github', type: 'ionicon' }}
-        title="Issues github"
-        subtitle="Un problème ? clique ici !"
-        onPress={() => Linking.openURL(GITHUB_LINK)}
-        bottomDivider
-      />
-      <ListItem
-        leftIcon={{ name: 'ios-log-out', type: 'ionicon' }}
-        title="Se deconnecter"
-        onPress={() => firebase.auth().signOut()}
-      />
+    <View style={styles.container}>
+      <View>
+        <ListItem
+          leftIcon={{ name: 'logo-github', type: 'ionicon' }}
+          title="Issues github"
+          subtitle="Un problème ? clique ici !"
+          onPress={() => Linking.openURL(GITHUB_LINK)}
+          bottomDivider
+        />
+        <ListItem
+          leftIcon={{ name: 'ios-log-out', type: 'ionicon' }}
+          title="Se deconnecter"
+          onPress={() => firebase.auth().signOut()}
+        />
+      </View>
     </View>
   );
 };
